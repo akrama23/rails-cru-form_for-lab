08c8ada9a661d8678475dd98c665cd2e38173249
akrama23 <arundelfootball23@gmail.com>
2020-12-13 01:07:34 -0500

    commit tree 54a72a6ba43ac9e51a5b39b56dcc900c67e12c49

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
+  # resources :artists, only: []
+
+  get '/artists', to: 'artists#index', as: :artists
+  get '/artists/new', to: 'artists#new', as: :new_artist
+  get '/artists/:id', to: 'artists#show', as: :artist
+  post '/artists', to: 'artists#create'
+  get '/artists/:id/edit', to: 'artists#edit', as: :edit_artist
+  patch '/artists/:id', to: 'artists#update'
+
+  resources :genres, only: [:index, :new, :show, :create, :edit, :update]
+  respurces :songs, only: [:index, :new, :show, :create, :edit, :update]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Artist.destroy_all 
+Genre.destroy_all 
+Song.destroy_all
+
+art1 = Artist.create(name: "artist1", bio: "artist1 bio")
+art2 = Artist.create(name: "artist2", bio: "artist2 bio")
+
+g1 = Genre.create(name: "Rock")
+g2 = Genre.create(name: "pop")
+
+Song.create(name: "song1", artist_id: art2.id, genre_id: g1.id)
+Song.create(name: "song2", artist_id: art1.id, genre_id: g2.id)
+Song.create(name: "song3", artist_id: art1.id, genre_id: g1.id)
+Song.create(name: "song4", artist_id: art2.id, genre_id: g2.id)
